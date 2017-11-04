18c37c733b66e79503254f2b499e2669d3a42677
Tomoka Baba <baba@tofulab.jp>
2017-11-04 19:34:08 +0900

    fix.

--- 01.php
+++ 01.php
@@ -3,6 +3,6 @@
 $str = 'パタトクカシーー';
 $result = '';
 for ($i = 0; $i < mb_strlen($str); ++$i) {
-    $result .= ($i % 2 === 1) ? mb_substr($str, $i, 1) : '';
+    $result .= ($i % 2 === 0) ? mb_substr($str, $i, 1) : '';
 }
 echo $result;
