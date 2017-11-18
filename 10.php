ea9a4c4211135ad2667607f8f5ed4a54ef876b91
Tomoka Baba <baba@tofulab.jp>
2017-11-19 02:11:22 +0900

    Complete 10.php

---
+++ 10.php
@@ -0,0 +1,4 @@
+<?php
+
+echo substr_count(file_get_contents($argv[1]), "\n");
+echo "\n";
--- 12.sh
+++ 12.sh
@@ -1,2 +1,2 @@
-cut -f 1 hightemp.txt > col1.txt
-cut -f 2 hightemp.txt > col2.txt
+cut -f 1 hightemp.txt
+cut -f 2 hightemp.txt
