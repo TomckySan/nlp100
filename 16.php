19315500f8f1db8e9d2e2b343e4674a5dffc3566
Tomoka Baba <baba@tofulab.jp>
2017-11-26 12:50:31 +0900

    Complete 16.php

---
+++ 16.php
@@ -0,0 +1,13 @@
+<?php
+
+$fileRows = explode("\n", file_get_contents($argv[1]));
+$contents = '';
+$fileNameCount = 0;
+for($i = 0; $i < count($fileRows); $i++) {
+    $contents .= ($fileRows[$i]."\n");
+    if (($i % $argv[2]) === ($argv[2] - 1)) {
+        file_put_contents('./hightemp16_'.$c.'.txt', $contents, LOCK_EX);
+        $contents = '';
+        $fileNameCount++;
+    }
+}
